66899a320ff7c85d0d2489b1d6008487172e86a8
James <gongxia20@sina.cn>
2018-09-11 17:45:54 +0800

    添加获取私有属性和私有方法

--- Test/ViewController.m
+++ Test/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <objc/runtime.h>
+#import <MediaPlayer/MediaPlayer.h>
 
 @interface ViewController ()
 
@@ -17,15 +19,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"----121212");
-    NSLog(@"----12121");
-    NSLog(@"----12121");
-    NSLog(@"=====");
-    NSLog(@"=====");
-    NSLog(@"-----");
-    NSLog(@"-----");
-    NSLog(@"-----");
+        [self getAllProperty:[MPVolumeView class]];
+        [self getAllFunction:[MPVolumeView class]];
+}
 
+// 获取私有属性
+- (void)getAllProperty:(Class)class {
+    unsigned int count;
+    objc_property_t *properties = class_copyPropertyList(class, &count);
+    
+    for (int index = 0; index < count; ++index) {
+        // 声明属性
+        objc_property_t property = properties[index];
+        
+        // 指针数组. 通过property_getName获取属性的名称
+        const char *cName = property_getName(property);
+        
+        NSString *name = [NSString stringWithUTF8String:cName];
+        NSLog(@"name -----> %@", name);
+    }
+    
+}
+
+// 获取私有方法
+- (void)getAllFunction:(Class)class {
+    
+    unsigned int count;
+    Method *methods = class_copyMethodList(class, &count);
+    
+    for (int index = 0; index < count; ++index) {
+        Method method = methods[index];
+        
+        // 这步完了之后, 可以获取到方法的地址
+        SEL selector = method_getName(method);
+        
+        NSString *name = NSStringFromSelector(selector);
+        const char *type = method_getTypeEncoding(method);
+        NSLog(@"fun-----%@,  type-----> %s",name,type);
+    }
 }
 
 @end
